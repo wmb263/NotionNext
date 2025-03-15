89e33fa02837a8454e6ada001d75cdf378ef1392
每文 <96628530+wmb263@users.noreply.github.com>
2025-03-16 00:27:14 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -20,9 +20,9 @@
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享编程',
   HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
+  HEO_HERO_TITLE_3: '"Hello world "',
+  HEO_HERO_TITLE_4: 'WMB',
+  HEO_HERO_TITLE_5: '热爱生活，知足常乐',
   HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
